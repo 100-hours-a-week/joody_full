c8670a320425867b9a67959f837a3513ca06bfca
Joody20 <loveed123@naver.com>
2025-10-16 23:58:25 +0900

    @ValidDTO수정

--- src/main/java/com/example/assignment_4/dto/PostRequest.java
+++ src/main/java/com/example/assignment_4/dto/PostRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -15,8 +16,6 @@ public class PostRequest {
 
     @NotBlank(message = "content_required")
     private String content;
+
+    private MultipartFile image;  // 이미지 파일도 포함
 }
-
-
-/* 여기서는 MultipartFile을 DTO에 직접 넣지 않고, Controller에서 따로 받는 방식으로 구현함.
-이게 multipart/form-data 처리 시 가장 안정적이고 에러가 적음 */
