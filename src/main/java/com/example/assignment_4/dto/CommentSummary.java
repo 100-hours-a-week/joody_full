335cf185bbc93ec1529eca48db68ccfb4e2d1aae
Joody20 <loveed123@naver.com>
2025-10-19 00:48:38 +0900

    DTO_어노테이션추가

--- src/main/java/com/example/assignment_4/dto/CommentSummary.java
+++ src/main/java/com/example/assignment_4/dto/CommentSummary.java
@@ -1,5 +1,6 @@
 package com.example.assignment_4.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "게시글에 대한 댓글 요약 DTO")
 public class CommentSummary {
+
+    @Schema(description = "댓글 ID", example = "10")
     private Long commentId;
+
+    @Schema(description = "댓글 작성자 닉네임", example = "joody")
     private String author;
+
+    @Schema(description = "댓글 내용", example = "좋은 정보 감사합니다!")
     private String content;
+
+    @Schema(description = "댓글 작성 일시", example = "2025-10-19T15:30:00")
     private String createdAt;
 }
