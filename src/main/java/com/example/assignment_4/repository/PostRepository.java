bcaff4ffc6294f6c02d5682aa7d96d02d04de22b
Joody20 <loveed123@naver.com>
2025-11-03 00:24:09 +0900

    Post레포지토리구현

--- src/main/java/com/example/assignment_4/repository/PostRepository.java
+++ src/main/java/com/example/assignment_4/repository/PostRepository.java
@@ -1,4 +1,18 @@
 package com.example.assignment_4.repository;
 
-public class PostRepository {
+import com.example.assignment_4.entity.Post;
+import com.example.assignment_4.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // 특정 유저가 작성한 게시글 조회
+    List<Post> findByUser(User user);
+
+    // 제목 검색 기능 (키워드 기반)
+    List<Post> findByTitleContaining(String keyword);
 }
