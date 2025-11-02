1d629d9bb86015f665ce70164faa3758b0fb505d
Joody20 <loveed123@naver.com>
2025-11-03 01:31:12 +0900

    댓글_dto_수정

--- src/main/java/com/example/assignment_4/dto/CommentSummary.java
+++ src/main/java/com/example/assignment_4/dto/CommentSummary.java
@@ -1,25 +1,34 @@
 package com.example.assignment_4.dto;
 
+import com.example.assignment_4.entity.Comment;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "게시글에 대한 댓글 요약 DTO")
+@Builder
+@Schema(description = "댓글 요약 DTO")
 public class CommentSummary {
 
-    @Schema(description = "댓글 ID", example = "10")
-    private Long commentId;
+    @Schema(description = "댓글 ID", example = "1")
+    private Long id;
 
-    @Schema(description = "댓글 작성자 닉네임", example = "joody")
+    @Schema(description = "작성자 닉네임", example = "joody")
     private String author;
 
-    @Schema(description = "댓글 내용", example = "좋은 정보 감사합니다!")
+    @Schema(description = "댓글 내용", example = "좋은 글이에요!")
     private String content;
 
-    @Schema(description = "댓글 작성 일시", example = "2025-10-19T15:30:00")
+    @Schema(description = "작성일", example = "2025-11-03T12:30:00")
     private String createdAt;
+
+    public static CommentSummary from(Comment comment) {
+        return CommentSummary.builder()
+                .id(comment.getId())
+                .author(comment.getUser().getNickname())
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt().toString())
+                .build();
+    }
 }
