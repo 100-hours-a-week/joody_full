199ac90a3078e0b55f42c1ab90c471c54732f202
Joody20 <loveed123@naver.com>
2025-11-03 21:28:45 +0900

    PostRepository정리

--- src/main/java/com/example/assignment_4/repository/PostRepository.java
+++ src/main/java/com/example/assignment_4/repository/PostRepository.java
@@ -10,13 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    // 특정 유저가 작성한 게시글 조회
-    List<Post> findByUser(User user);
-
-    // 제목 검색 기능 (키워드 기반)
-    List<Post> findByTitleContaining(String keyword);
-
     Optional<Post> findByIdAndDeletedAtIsNull(Long id);
-
 }
