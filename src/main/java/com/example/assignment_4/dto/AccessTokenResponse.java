e5a381a775d5030d8cbe6cedd651b83795a825d8
Joody20 <loveed123@naver.com>
2025-11-28 10:05:26 +0900

    style: 네임 수정

--- src/main/java/com/example/assignment_4/dto/AccessTokenResponse.java
+++ src/main/java/com/example/assignment_4/dto/AccessTokenResponse.java
@@ -1,4 +1,10 @@
 package com.example.assignment_4.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AccessTokenResponse {
+    private String accessToken;
 }
